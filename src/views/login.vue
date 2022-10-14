0d17b3c83cb69e4b1db50a18b835bd296ccd424a
lxswd <lsxwd169095@gmail.com>
2022-10-14 17:15:59 +0800

    提交，20221014

---
+++ src/views/login.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="login">
+        <el-card class="box-card">
+            <el-form ref="loginForm" :model="loginForm" :rules="rules">
+                <el-form-item prop="username">
+                    <el-input placeholder="请输入账号" v-model="loginForm.username" clearable></el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input placeholder="请输入密码" v-model="loginForm.password" show-password></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary">登陆</el-button>
+                </el-form-item>
+            </el-form>
+        </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            loginForm: {
+                username: '',
+                password: ''
+            },
+            rules: {
+                username: [
+                    {require: true, message: '请输入账号', trigger: 'blur'}
+                ],
+                password: [
+                    {require: true, message: '请输入密码', trigger: 'blur'}
+                ]
+            }
+        }
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="scss" socped>
+.login {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgb(203, 197, 197);
+}
+.box-card {
+    width: 400px;
+    // margin: 0 auto;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    .el-input {
+        margin-bottom: 10px;
+    }
+    .el-button {
+        width: 100%;
+    }
+}
+</style>
